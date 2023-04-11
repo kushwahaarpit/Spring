f508cf450722e13f46152bb45574f42e58ef5edf
kushwahaarpite <arpit29062k@gmail.com>
2023-04-11 17:21:35 +0530

    Storing user credential- InMemory

--- src/main/java/com/learn/security/springsecurity/basicConfiguration/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/learn/security/springsecurity/basicConfiguration/BasicAuthSecurityConfiguration.java
@@ -5,13 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    SecurityFilterChain securityFilterChain(HttpSecurity  http) throws Exception
     {
         http.authorizeHttpRequests(
                 auth -> {
@@ -26,5 +30,20 @@ public class BasicAuthSecurityConfiguration {
 //        http.formLogin();
         http.httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        var user = User.withUsername("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+        var admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
